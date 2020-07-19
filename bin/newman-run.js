9d24a7f2360a993a06553b97d57e745000f19bce
Naresh Sekar <naresh@madstreetden.com>
2020-07-19 22:35:02 +0530

    first working package push

--- bin/newman-run.js
+++ bin/newman-run.js
@@ -11,7 +11,7 @@ const yargs = require("yargs");
 const { option } = require('yargs');
 var feedFilePath = ""
 
-clear();
+
 console.log(
     chalk.rgb(220, 120, 60)(
         figlet.textSync('Newman-Run', { horizontalLayout: 'full' })
@@ -20,21 +20,10 @@ console.log(
 
 const options = yargs
         .usage("Usage: newman-run -f <feed_file_path>")
-        .option("f", { alias: "feed", describe: "Feed file path", type: "string" })
+        .option("f", { alias: "feed", describe: "Feed file path", type: "string", required: true})
         .argv
 
-if (option.feed == undefined) {
-    const run = async () => {
-        const credentials = await inquirer.askForFeedFile();
-        feedFilePath = credentials.feed
-        console.log('Feed file taken is: ' + credentials.feed);
-    };
-    run();
-} else {
-    feedFilePath = option.feed
-    console.log('Feed file taken is: ' + option.feed);
-}
+feedFilePath = options.feed
+console.log('Feed file taken is: ' + feedFilePath);
 
 new NewmanConfig(feedFilePath)
-
-
