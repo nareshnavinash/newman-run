626c2dc803817ec295f67d74768f611f74c98bda
Naresh Sekar <naresh@madstreetden.com>
2020-07-21 10:56:14 +0530

    adding -r to remove previous report files

--- bin/newman-run.js
+++ bin/newman-run.js
@@ -22,18 +22,22 @@ console.log(
     )
 );
 
-const file_error_message = chalk.red.bold('Need either feed file (-f) or collections (-c) file to run the tests !!!\n')
+const file_error_message = chalk.red.bold('Need either feed file (-f) or collections (-c) file to run the tests or atleast (-r) to remove the files from reports directory!!!\n')
 
 const options = yargs
         .usage("Usage: newman-run -f <feed_file_path>")
         .option("f", { alias: "feed", describe: "Feed file path", type: "string"})
         .option("c", { alias: "collection", describe: "Collection file path file path", type: "string"})
         .option("e", { alias: "environment", describe: "Environment file path file path", type: "string"})
-        .check(argv => { if(argv.f == undefined && argv.c == undefined) { console.log(file_error_message); return false } else { return true }})
+        .option("r", { alias: "remove", describe: "To remove the files from reporting directory"})
+        .check(argv => { if(argv.f == undefined && argv.c == undefined && argv.r == undefined) { console.log(file_error_message); return false } else { return true }})
         .argv
 
 const NC = new NewmanConfig()
 
+if (options.remove) {
+    NC.clearResultsFolder()
+}
 if (options.feed != undefined && options.collection == undefined && options.environment == undefined) {
     NC.looprun(options.feed)
 } else if (options.collection != undefined && options.environment == undefined && options.feed == undefined) {
